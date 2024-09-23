81aae27c963324f3c5ea41ee621a68ab36604a38
Nick Ng <nick-ng@users.noreply.github.com>
2024-09-23 19:03:37 +1200

    feat: add more video player controls. use display area better

--- src/components/video-player/index.tsx
+++ src/components/video-player/index.tsx
@@ -22,11 +22,13 @@ export default function VideoPlayer() {
   const { options, setOptions } = useOptions();
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
   const [videoStarted, setVideoStarted] = useState(false);
-  const [timingString, setTimingString] = useState("");
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(1);
 
   const containerRef = useRef<HTMLDivElement | null>(null);
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const leftControlsRef = useRef<HTMLDivElement | null>(null);
+  const bottomControlsRef = useRef<HTMLDivElement | null>(null);
   const resizeTimeoutRef = useRef<number | null>(null);
   const videoPropertiesRef = useRef<{ seeking: boolean }>({ seeking: false });
 
@@ -43,19 +45,20 @@ export default function VideoPlayer() {
         videoRef.current?.videoWidth
       ) {
         const aspectRatio =
-          videoRef.current?.videoWidth / videoRef.current?.videoHeight;
-
-        const width1 = containerRef.current.offsetWidth;
-        const height1 = width1 / aspectRatio;
-
-        const height2 = containerRef.current.offsetHeight;
-        const width2 = aspectRatio * height2;
+          (videoRef.current?.videoWidth || 1) /
+          (videoRef.current?.videoHeight || 1);
 
         const controlsWidth = leftControlsRef.current?.offsetWidth || 50;
+        const width1 = containerRef.current.offsetWidth - controlsWidth * 2;
+        const height1 = width1 / aspectRatio;
 
-        const width = Math.min(width1, width2) - controlsWidth * 2;
+        const progressControlsHeight =
+          bottomControlsRef.current?.offsetHeight || 20;
+        const height2 =
+          containerRef.current.offsetHeight - progressControlsHeight;
+        const width2 = aspectRatio * height2;
 
-        console.log("width", width);
+        const width = Math.min(width1, width2);
 
         setDimensions({
           width,
@@ -111,11 +114,9 @@ export default function VideoPlayer() {
         return;
       }
 
-      setTimingString(
-        `${formatSeconds(videoRef.current.currentTime)} / ${formatSeconds(
-          videoRef.current.duration
-        )}`
-      );
+      setCurrentTime(videoRef.current.currentTime);
+      setDuration(videoRef.current.duration);
+      setVideoStarted(!videoRef.current.paused);
     }, 333);
 
     return () => {
@@ -132,7 +133,7 @@ export default function VideoPlayer() {
 
   return (
     <div
-      className="relative flex h-screen w-screen items-center justify-center bg-gray-800"
+      className="relative flex h-screen w-screen items-start justify-center bg-gray-800"
       ref={containerRef}
     >
       {!filename && (
@@ -140,14 +141,17 @@ export default function VideoPlayer() {
           Video not found. <Link to="/">Go back to video list</Link>
         </div>
       )}
-      <div className="flex flex-col justify-center" ref={leftControlsRef}>
+      <div
+        className="flex flex-col justify-center self-center"
+        ref={leftControlsRef}
+      >
         <button
           className="border border-white px-2 py-4 text-white"
           onClick={() => {
-            onSeek(-1);
+            onSeek(-5);
           }}
         >
-          1s
+          5s
         </button>
         <button
           className="border border-white px-2 py-4 text-white"
@@ -173,6 +177,56 @@ export default function VideoPlayer() {
         >
           5m
         </button>
+        <button
+          className="border border-white px-2 py-4 text-white"
+          onClick={() => {
+            if (videoRef.current) {
+              videoRef.current.pause();
+              videoRef.current.currentTime = 0;
+            }
+          }}
+        >
+          ⏹️
+        </button>
+        <input
+          style={{ writingMode: "vertical-lr", direction: "rtl" }}
+          type="range"
+          value={videoRef.current?.volume || 0.5}
+          max={1}
+          step={0.01}
+          onInput={(e) => {
+            if (videoRef.current) {
+              videoRef.current.volume = parseFloat(e.currentTarget.value);
+            }
+          }}
+          onWheel={(e) => {
+            if (!videoRef.current) {
+              return;
+            }
+            console.log("wheel volume", e.deltaY);
+            console.log(
+              "wheel key",
+              e.shiftKey,
+              e.altKey,
+              e.ctrlKey,
+              e.metaKey
+            );
+
+            if (e.deltaY === 0) {
+              return;
+            }
+
+            let volumeDelta = e.deltaY > 0 ? -0.05 : 0.05;
+
+            if (e.shiftKey || e.altKey || e.ctrlKey || e.metaKey) {
+              volumeDelta = volumeDelta / 5;
+            }
+
+            const newVolume = videoRef.current.volume + volumeDelta;
+
+            videoRef.current.volume = Math.max(0, Math.min(newVolume, 1));
+          }}
+        />
       </div>
       {filename && (
         <video
@@ -196,16 +250,17 @@ export default function VideoPlayer() {
           controls
           width={dimensions.width || 100}
           height={dimensions.height || 100}
+          preload="auto"
         />
       )}
-      <div className="flex flex-col justify-center">
+      <div className="flex flex-col justify-center self-center">
         <button
           className="border border-white px-2 py-4 text-white"
           onClick={() => {
-            onSeek(1);
+            onSeek(5);
           }}
         >
-          1s
+          5s
         </button>
         <button
           className="border border-white px-2 py-4 text-white"
@@ -231,6 +286,30 @@ export default function VideoPlayer() {
         >
           5m
         </button>
+        {!videoStarted && (
+          <button
+            className="border border-white px-2 py-4 text-white"
+            onClick={() => {
+              if (videoRef.current) {
+                videoRef.current.play();
+              }
+            }}
+          >
+            ▶️
+          </button>
+        )}
+        {videoStarted && (
+          <button
+            className="border border-white px-2 py-4 text-white"
+            onClick={() => {
+              if (videoRef.current) {
+                videoRef.current.pause();
+              }
+            }}
+          >
+            ⏸️
+          </button>
+        )}
       </div>
       <div
         className={`absolute top-0 left-0 border border-gray-900 bg-white p-3 transition-opacity hover:opacity-100 ${
@@ -240,8 +319,24 @@ export default function VideoPlayer() {
         <Link to="/">Back to Video List</Link>
         <p>You can press Space to pause or unpause the video.</p>
       </div>
-      <div className="absolute bottom-0 left-0 right-0 m-auto text-center text-white">
-        {timingString}
+      <div
+        className="absolute bottom-0 left-0 right-0 m-auto flex flex-row justify-between gap-4 px-4 text-center text-white"
+        ref={bottomControlsRef}
+      >
+        <span>{formatSeconds(currentTime)}</span>
+        <input
+          className="flex-grow"
+          type="range"
+          value={videoRef.current?.currentTime || 0}
+          max={videoRef.current?.duration}
+          step={0.01}
+          onInput={(e) => {
+            if (videoRef.current) {
+              videoRef.current.currentTime = parseFloat(e.currentTarget.value);
+            }
+          }}
+        />
+        <span>{formatSeconds(duration)}</span>
       </div>
     </div>
   );
