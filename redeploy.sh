5f979b6aefa4a89538609db5ede60de075dbfdf4
Nick Ng <nick-ng@users.noreply.github.com>
2024-09-25 18:28:45 +1200

    chore: remove unnecessary npm commands from redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -1,7 +1,6 @@
 git checkout -f main
 git pull
-npm install
-npm run build
+
 PORT=3012 docker compose -f docker-compose.yml down
-docker compose -f docker-compose.yml build
+PORT=3012 docker compose -f docker-compose.yml build
 PORT=3012 docker compose -f docker-compose.yml up -d
--- redeploy80.sh
+++ redeploy80.sh
@@ -1,7 +1,6 @@
 git checkout -f main
 git pull
-npm install
-npm run build
+
 sudo PORT=80 docker compose -f docker-compose.yml down
 sudo PORT=80 docker compose -f docker-compose.yml build
 sudo PORT=80 docker compose -f docker-compose.yml up -d
